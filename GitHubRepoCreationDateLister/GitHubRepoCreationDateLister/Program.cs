579f719d2f7adfe0dd6dd5094f6855bf916ecf86
Akira Kawamata <autumn@piedey.co.jp>
2023-02-25 14:53:33 +0900

    temp save

--- GitHubRepoCreationDateLister/GitHubRepoCreationDateLister/Program.cs
+++ GitHubRepoCreationDateLister/GitHubRepoCreationDateLister/Program.cs
@@ -2,6 +2,7 @@
 
 if ( args.Length != 2) {
     Console.WriteLine("usage: gh repo list -L 1000|GitHubRepoCreationDateLister INPUT_FILE_NAME  OUTPUT_FILE_NAME");
+    Console.WriteLine("use 'gh auth login' before use");
     return;
 }
 
@@ -15,15 +16,13 @@ using (var reader = System.IO.File.OpenText(args[0]))
         var s = reader.ReadLine();
         if (s == null) break;
         var d = s.Split('\t');
-        list.Add(new Repo() { Name = getName(d[0]), Desc = d[1] });
+        list.Add(new Repo() {FullName=d[0], Name = getName(d[0]), Desc = d[1], Created = await getCreated(d[0]) });
     }
 }
 
-// TBW
-
 using (var writer = System.IO.File.CreateText(args[1]))
 {
-    foreach (var item in list)
+    foreach (var item in list.OrderByDescending(c=>c.Created))
     {
         writer.WriteLine($"{item.Name} {item.Desc}");
     }
@@ -41,7 +40,33 @@ string getName(string s) {
     return s.Substring(index + 1);
 }
 
+async Task<string> getCreated(string fullName)
+{
+    var all = await detailLoader(fullName);
+    int index = all.IndexOf("created_at");
+    int index2 = all.IndexOf("2", index);
+    int index3 = all.IndexOf("Z", index2);
+    return all.Substring(index2, index3 - index2 + 1);
+}
+
+async Task<string> detailLoader(string fullName)
+{
+    var command = @" api https://api.github.com/repos/" + fullName;
+    await Console.Out.WriteLineAsync(command);
+    ProcessStartInfo pi = new ProcessStartInfo()
+    {
+        FileName = "gh",
+        Arguments = command,
+        RedirectStandardOutput = true,
+    };
+    var process = Process.Start(pi);
+    var r = process.StandardOutput.ReadToEnd();
+    process.WaitForExit();
+    return r;
+}
+
 class Repo {
+    public string? FullName { get; set; }
     public string? Name { get; set; }
     public string? Desc { get; set; }
     public string? Created { get; set; }
